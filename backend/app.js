e17cdc4ee6812df7ffcf1b0872bd96ac946d8923
Kartikey Saxena <kartikeysaxena2030@gmail.com>
2024-08-10 16:35:50 +0530

    commit

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { errorMiddleware } from "./error/error.js";
+import { errorMiddleware } from "./middlewares/error.js";
 import reservationRouter from "./routes/reservationRoute.js";
 import { dbConnection } from "./database/dbConnection.js";
 
 const app = express();
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config.env" });
 
-// Set up CORS to allow only the React app to access the backend
-app.use(cors({
-  origin: [process.env.FRONTEND_URL], // Allows your React app based on the environment variable
-  methods: ["POST"], // Restricting to POST requests
-  credentials: true, // Allow credentials such as cookies to be sent
-}));
-
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["POST"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Route to handle reservation-related requests
 app.use("/api/v1/reservation", reservationRouter);
+app.get("/", (req, res, next)=>{return res.status(200).json({
+  success: true,
+  message: "HELLO WORLD AGAIN"
+})})
 
-// Establish a database connection
 dbConnection();
 
-// Error handling middleware
 app.use(errorMiddleware);
 
 export default app;
